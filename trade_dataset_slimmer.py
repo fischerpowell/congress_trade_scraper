09fc540cd92ba2c9eea8f493a95772ffbaac0c36
Sachin-Buluswar <156264263+Sachin-Buluswar@users.noreply.github.com>
2024-01-18 19:28:55 -0500

    Update trade_dataset_slimmer.py

--- trade_dataset_slimmer.py
+++ trade_dataset_slimmer.py
@@ -1,4 +1,9 @@
+'''
+This code takes in the JSON file of senator trades, removes certain columns, 
+saves it as a JSON file, and then aditionally as a CSV without duplicates.
+'''
 import json
+import pandas as pd
 
 with open("congress_trading_dataset.json", "r") as f:
     dataset = json.load(f)
@@ -19,4 +24,18 @@ for record in dataset:
 
 with open("slimmed_trading_dataset.json", "w") as new_file:
     json.dump(new_dataset, new_file)
+
+
+# Opens JSON file
+f = open("slimmed_trading_dataset.json")
+ 
+# converts JSON into dictionary 
+data = json.load(f)
+
+new_data = pd.DataFrame(data)
+
+
+
+new_data.drop_duplicates(inplace=True)
+new_data.to_csv("slimmed_trading_dataset.csv", index=False)
 
